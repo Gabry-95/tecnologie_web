f1d47f0d3dff8457949799372859efbe035f8b0d
gabri <gabri@DESKTOP-TN1KACF.wind3.hub>
2026-04-25 17:12:05 +0200

    db manager e libreria

--- src/it/pale/tweb/dao/beans/ClienteDAO.java
+++ src/it/pale/tweb/dao/beans/ClienteDAO.java
@@ -1,7 +1,12 @@
 package it.pale.tweb.dao.beans;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Vector;
+import java.sql.Connection;
+import it.pale.tweb.dao.utils.DBManager;
+
 public class ClienteDAO {
-	public static void main (String[] args) {
-		System.out.print("siamo nel branch");
-	}
+	
 }
