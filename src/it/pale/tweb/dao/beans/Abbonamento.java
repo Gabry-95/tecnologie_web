a25243e00d6d0f69f01dea6e1b4953dd534b2c30
gabri <gabri@DESKTOP-TN1KACF.wind3.hub>
2026-04-26 11:29:46 +0200

    modifiche effettuate: -abbonamento -abbonamento dao -da accesso eliminato -da accesso dao da completare

---
+++ src/it/pale/tweb/dao/beans/Abbonamento.java
@@ -0,0 +1,96 @@
+package it.pale.tweb.dao.beans;
+
+import java.util.Date;
+
+public class Abbonamento {
+	private int fattura;
+	private String tipo;
+	private Date dataScadenza;
+	private Integer limiteIngressi;
+	private int ingressi;
+	private int costo;
+	private int cliente;
+	
+	public Abbonamento() {
+		this.fattura = 0;
+		this.tipo = null;
+		this.dataScadenza = null;
+		this.limiteIngressi = null;
+		this.ingressi = 0;
+		this.costo = 0;
+		this.cliente = 0;
+	}
+
+	public Abbonamento(int fattura, String tipo, Date dataScadenza, int ingressi, int costo,
+			int cliente) {
+		this.fattura = fattura;
+		this.tipo = tipo;
+		this.dataScadenza = dataScadenza;
+		if(this.tipo.equals("studente")) {
+			this.limiteIngressi = 3;
+		}
+		else {
+			//CONVENZIONE, SE IL PIANO DI ABBONAMENTO NON PREVEDE LIMITI DI INGRESSI VIENE SETTATO A ZERO
+			this.limiteIngressi = null;
+		}
+		this.ingressi = ingressi;
+		this.costo = costo;
+		this.cliente = cliente;
+	}
+
+	public int getFattura() {
+		return fattura;
+	}
+	
+	public void setFattura(int fattura) {
+		this.fattura = fattura;
+	}
+	
+	public String getTipo() {
+		return tipo;
+	}
+	
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	public Date getDataScadenza() {
+		return dataScadenza;
+	}
+	
+	public void setDataScadenza(Date dataScadenza) {
+		this.dataScadenza = dataScadenza;
+	}
+	
+	public Integer getLimiteIngressi() {
+		return limiteIngressi;
+	}
+
+	public void setLimiteIngressi(Integer limiteIngressi) {
+		this.limiteIngressi = limiteIngressi;
+	}
+
+	public int getIngressi() {
+		return ingressi;
+	}
+	
+	public void setIngressi(int ingressi) {
+		this.ingressi = ingressi;
+	}
+	
+	public int getCosto() {
+		return costo;
+	}
+	
+	public void setCosto(int costo) {
+		this.costo = costo;
+	}
+	
+	public int getCliente() {
+		return cliente;
+	}
+	
+	public void setCliente(int cliente) {
+		this.cliente = cliente;
+	}
+}
